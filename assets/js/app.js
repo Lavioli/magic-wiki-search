de78ec38647d4dd000c59426ce4562b97761ed59
LavieMaru <laviemaru@gmail.com>
2016-08-31 20:28:12 -0400

    Work on ajax method to have array from Wiki to run through Bing API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,103 +1,133 @@
-function fetchFromWiki(searchTerm){
-  $.ajax({
-    method: 'get',
-    url: 'https://en.wikipedia.org/w/api.php',
-    data: {
-      format: 'json',
-      action: 'opensearch',
-      search: searchTerm
-    },
-    dataType: 'jsonp'
-  })
+function fetchFromWiki(searchTerm) {
+    $.ajax({
+            method: 'get',
+            url: 'https://en.wikipedia.org/w/api.php',
+            data: {
+                format: 'json',
+                action: 'opensearch',
+                search: searchTerm
+            },
+            dataType: 'jsonp'
+        })
+        //returns an array
 
-  .then(function(data) {
-    console.dir(data);
-    renderSearchResults(data[1], data[3]);
-  })
 
-.done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
-});
+    .done(function(data) {
+        $(function() {
+        var params = {
+            q: 'cat',
+            count: 1// Request parameters
+        };
+      
+        $.ajax({
+            url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","multipart/form-data");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","4d987553b4a14267861b431e1c2779ef");
+            },
+            type: "POST",
+            // Request body
+            // data: "{body}",
+        })
+        .done(function(data) {
+             console.log(data);
+              console.log(data.value[0].contentUrl);
+        })
+        .fail(function() {
+            alert("error");
+        });
+    });
+             
+    })
 
-$.ajax({
-  method: 'get',
-  url: 'https://en.wikipedia.org/w/api.php',
-  data: {
-   
-      format: 'json',
-      action: 'query',
-      titles: searchTerm,
-      prop: 'images'
-    },
-    dataType: 'jsonp'
-})
-.then(function(data) {
-    console.dir(data);
-  }).done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
+    .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
 
-});
-/*
 
-$.ajax({
-  method: 'get',
-  url: 'https://commons.wikimedia.org/w/api.php',
-  data: {
-      action: 'query',
-      title: searchTerm,
-      prop: 'image',
-      iiprop: 'url'
 
-    },
-    dataType: 'jsonp'
-})
-.then(function(data) {
-    console.dir(data);
-  }).done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
 
-});*/
+    /*$.ajax({
+      method: 'get',
+      url: 'https://en.wikipedia.org/w/api.php',
+      data: {
+       
+          format: 'json',
+          action: 'query',
+          titles: searchTerm,
+          prop: 'images'
+        },
+        dataType: 'jsonp'
+    })
+    .then(function(data) {
+        console.dir(data);
+      }).done(function(data) {
+      console.dir(data);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+
+    });*/
+    /*
+
+    $.ajax({
+      method: 'get',
+      url: 'https://commons.wikimedia.org/w/api.php',
+      data: {
+          action: 'query',
+          title: searchTerm,
+          prop: 'image',
+          iiprop: 'url'
+
+        },
+        dataType: 'jsonp'
+    })
+    .then(function(data) {
+        console.dir(data);
+      }).done(function(data) {
+      console.dir(data);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+
+    });*/
+
 };
 
 
 
 
 
-function renderSearchResults(listOfLabels, listOfUrls){
-  var html = "";
-  $.each(listOfLabels, function(index, item){
-    var url = listOfUrls[index];
-    html += "<li><a target='_blank' href='" + url + "'>" + item + "</a></li>";
-  });
-  $('.search-results').html(html);
+
+function renderSearchResults(listOfLabels, listOfUrls) {
+    var html = "";
+    $.each(listOfLabels, function(index, item) {
+        var url = listOfUrls[index];
+        var img = fetchFromBing(item);
+        html += "<li><a target='_blank' href='" + url + "'>" + item + "</a> <img src='" + img + "'></li>";
+
+    });
+    $('.search-results').html(html);
 }
 
 
 // EVENT LISTENERS:
-$(function(){
+$(function() {
 
-  $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#query').val();
-    fetchFromWiki(searchTerm);
-  });
+    $('#search-term').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        fetchFromWiki(searchTerm);
+    });
 
 });
