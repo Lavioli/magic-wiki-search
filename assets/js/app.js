7a0d334a77927a9892308e0ddd2ad91db867a3ed
bealearnscode <bealearnscode@gmail.com>
2016-09-02 15:24:24 -0400

    remove padding-bottom from body add event listeners to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,103 +1,108 @@
-function fetchFromWiki(searchTerm){
-  $.ajax({
-    method: 'get',
-    url: 'https://en.wikipedia.org/w/api.php',
-    data: {
-      format: 'json',
-      action: 'opensearch',
-      search: searchTerm
-    },
-    dataType: 'jsonp'
-  })
+function fetchFromWiki(searchTerm) {
+    $.ajax({
+        method: 'get',
+        url: 'https://en.wikipedia.org/w/api.php',
+        data: {
+            format: 'json',
+            action: 'opensearch',
+            search: searchTerm
+        },
+        dataType: 'jsonp'
+    })
 
-  .then(function(data) {
-    console.dir(data);
-    renderSearchResults(data[1], data[3]);
-  })
+    .then(function(data) {
+        console.dir(data);
+        renderSearchResults(data[1], data[3]);
+    })
 
-.done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
-});
+    .done(function(data) {
+            console.dir(data);
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
 
-$.ajax({
-  method: 'get',
-  url: 'https://en.wikipedia.org/w/api.php',
-  data: {
-   
-      format: 'json',
-      action: 'query',
-      titles: searchTerm,
-      prop: 'images'
-    },
-    dataType: 'jsonp'
-})
-.then(function(data) {
-    console.dir(data);
-  }).done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
+    $.ajax({
+            method: 'get',
+            url: 'https://en.wikipedia.org/w/api.php',
+            data: {
 
-});
-/*
+                format: 'json',
+                action: 'query',
+                titles: searchTerm,
+                prop: 'images'
+            },
+            dataType: 'jsonp'
+        })
+        .then(function(data) {
+            console.dir(data);
+        }).done(function(data) {
+            console.dir(data);
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
 
-$.ajax({
-  method: 'get',
-  url: 'https://commons.wikimedia.org/w/api.php',
-  data: {
-      action: 'query',
-      title: searchTerm,
-      prop: 'image',
-      iiprop: 'url'
+        });
+    /*
 
-    },
-    dataType: 'jsonp'
-})
-.then(function(data) {
-    console.dir(data);
-  }).done(function(data) {
-  console.dir(data);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
+    $.ajax({
+      method: 'get',
+      url: 'https://commons.wikimedia.org/w/api.php',
+      data: {
+          action: 'query',
+          title: searchTerm,
+          prop: 'image',
+          iiprop: 'url'
 
-});*/
+        },
+        dataType: 'jsonp'
+    })
+    .then(function(data) {
+        console.dir(data);
+      }).done(function(data) {
+      console.dir(data);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+
+    });*/
 };
 
 
 
 
 
-function renderSearchResults(listOfLabels, listOfUrls){
-  var html = "";
-  $.each(listOfLabels, function(index, item){
-    var url = listOfUrls[index];
-    html += "<li><a target='_blank' href='" + url + "'>" + item + "</a></li>";
-  });
-  $('.search-results').html(html);
+function renderSearchResults(listOfLabels, listOfUrls) {
+    var html = "";
+    $.each(listOfLabels, function(index, item) {
+        var url = listOfUrls[index];
+        html += "<li><a target='_blank' href='" + url + "'>" + item + "</a></li>";
+    });
+    $('.search-results').html(html);
 }
 
 
 // EVENT LISTENERS:
-$(function(){
+$(function() {
 
-  $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#query').val();
-    fetchFromWiki(searchTerm);
-  });
+    $('#search-term').submit(function(event) {
+        event.preventDefault();
+        $('#search-div').hide();
+        $('#show-search-page').toggle();
+        $('.search-results').fadeIn(5000, function() {
+            var searchTerm = $('#query').val();
+            fetchFromWiki(searchTerm);
+        });
+
+    });
 
 });
