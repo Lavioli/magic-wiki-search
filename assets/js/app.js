58bb9263bcb2a364fb81041e629bbb9afe435152
LavieMaru <laviemaru@gmail.com>
2016-09-02 20:36:55 -0400

    edit fetFromWiki and clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
+//function takes in the searchTerm, which is the text from the text input #query
 function fetchFromWiki(searchTerm) {
+    //sends request by GET wit hthe following parameters
     $.ajax({
         method: 'get',
         url: 'https://en.wikipedia.org/w/api.php',
@@ -11,12 +13,14 @@ function fetchFromWiki(searchTerm) {
     })
 
     .then(function(data) {
-        console.dir(data);
-        renderSearchResults(data[1], data[3]);
+        //once target data is received, renderSearch results gets invoked
+        //data[1] is the array all the search result labels
+        //data[3] is the array with all the url to the search result label
+        renderSearchResults(data[1], data[3]); 
     })
-
+    //the below methods pretty much do nothing unless something is wrong
     .done(function(data) {
-            console.dir(data);
+            console.log("done");
         })
         .fail(function() {
             console.log("error");
@@ -24,70 +28,19 @@ function fetchFromWiki(searchTerm) {
         .always(function() {
             console.log("complete");
         });
-
-    $.ajax({
-            method: 'get',
-            url: 'https://en.wikipedia.org/w/api.php',
-            data: {
-
-                format: 'json',
-                action: 'query',
-                titles: searchTerm,
-                prop: 'images'
-            },
-            dataType: 'jsonp'
-        })
-        .then(function(data) {
-            console.dir(data);
-        }).done(function(data) {
-            console.dir(data);
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-
-        });
-    /*
-
-    $.ajax({
-      method: 'get',
-      url: 'https://commons.wikimedia.org/w/api.php',
-      data: {
-          action: 'query',
-          title: searchTerm,
-          prop: 'image',
-          iiprop: 'url'
-
-        },
-        dataType: 'jsonp'
-    })
-    .then(function(data) {
-        console.dir(data);
-      }).done(function(data) {
-      console.dir(data);
-    })
-    .fail(function() {
-      console.log("error");
-    })
-    .always(function() {
-      console.log("complete");
-
-    });*/
 };
 
-
-
-
-
+//function renders all the labels and url and concatenates to the ul as a li 
 function renderSearchResults(listOfLabels, listOfUrls) {
     var html = "";
+    //each function takes in the label and concatenates the item, which is the listOfLabels to the ul
     $.each(listOfLabels, function(index, item) {
-        var url = listOfUrls[index];
+        var url = listOfUrls[index]; //url
         html += "<li><a target='_blank' href='" + url + "'>" + item + "</a></li>";
     });
+    //the html variable gets changed 
     $('.search-results').html(html);
+    //when a is hovered over the search results, a minipreview will load for that result
     $('.search-results a').miniPreview({ prefetch: 'none' });
 }
 
@@ -98,8 +51,8 @@ $(function() {
     $('#search-term').submit(function(event) {
         event.preventDefault();
         $('#search-page').hide();
-        $('#results-page').toggle();
-        var searchTerm = $('#query').val();
+        $('#results-page').toggle(); //becomes show;
+        var searchTerm = $('#query').val(); 
         fetchFromWiki(searchTerm);
     });
 
